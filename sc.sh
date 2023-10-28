d1c82be1ae917f9beb244ff6897483edc86a0e0d
Sher-my <113996705+Sher-my@users.noreply.github.com>
2023-10-28 08:47:04 +0800

    Update sc.sh

--- sc.sh
+++ sc.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 ###This sctipt simply deals with single cell data
 wkd=/home/lf-3/yt/rawdatas/sc_10/
-cd ${wkd}
-###Build ref index
-STAR --runMode genomeGenerate --genomeDir index --runThreadN 16 --genomeFastaFiles /home/lf-3/yt/rawdatas/sc_10/raw_genome_gtf/GRCh38.P13.genome.fa --sjdbGTFfile /home/lf-3/yt/rawdatas/sc_10/raw_genome_gtf/gencode.v38.annotation.gtf --sjdbOverhang 149
-###Mapping
-STAR --runThreadN 16 --genomeDir /lf-3/yt/rawdatas/sc_10/ --readFilesIn /home/lf-3/yt/rawdatas/sc_10/samplename_S1_L004_R1_001.fastq.gz /home/lf-3/yt/rawdatas/sc_10/samplename_S1_L004_R2_001.fastq.gz --readFilesCommand gunzip -c --outFileNamePrefix /home/lf-3/yt/rawdatas/sc_10/raw_out/ --outSAMtype BAM SortedByCoordinate --quantMode GeneCounts --sjdbGTFfile /home/lf-3/yt/rawdatas/sc_10/raw-genes.gtf
+sample=sample_name
+###Get ref index from https://www.10xgenomics.com/support/software/space-ranger/downloads or made by yourself:
+cellranger mkref --genome=${wkd}/ref/mm10_cellranger --nthreads=10 --fasta= ${wkd}/ref/mm10.fa --genes=${wkd}/ref/mm10.refGene.gtf
 ###Matric
-cellranger count --id = sample1 --transcriptome = /lf-3/yt/rawdatas/sc_10/ --fastqs = /home/lf-3/yt/rawdatas/sc_10/ --sample = samplename --expect-cells = 1000 --nosecondary
+cd ${wkd}
+for s in ${sample}
+do
+	cellranger count --id=${s}_cellranger_count --fastqs=${wkd} --sample=${s} --transcriptome=${wkd}/ref/refdata-gex-mm10-2020-A --r1-length 26
+done
